86532e396b10a18be3e00ef467ae10a63f4d5cce
alexFiorenza <alexfiorenza2012@gmail.com>
2022-04-22 08:57:20 -0300

    Finished 12 and 13 exercises

---
+++ src/1.2.1.12.cpp
@@ -0,0 +1,18 @@
+// 1.2.1.12. Producto mediante sumas sucesivas
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, result = 0;
+    cout << "Introduzca dos valores numericos: " << endl;
+    cin >> a >> b;
+    int counter = 0;
+    while (counter < b)
+    {
+        // 5*3 = 5+5+5
+        result = a + result;
+        counter++;
+    }
+    cout << "Su producto mediante sumas sucesivas es: " << result << endl;
+    return 0;
+}
---
+++ src/1.2.1.13.cpp
@@ -0,0 +1,12 @@
+// Lección 1
+// 1.2.1.13. Factorial de un número
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout << "Ingrese un valor entero: " << endl;
+    cin >> a;
+    return 0;
+}
